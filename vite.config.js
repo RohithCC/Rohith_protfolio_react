2de2059f17b4e94f70e7b4ed1e000f6b950f13d6
RohithCC <nrohith775@gmail.com>
2025-10-28 12:45:03 +0530

    28-10-25 last commit 2nd

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // You can disable the red error overlay if you want:
-  // server: { hmr: { overlay: false } },
+  build: { outDir: "build" },   // 👈 change output folder
+  base: "/",                    // good default for root-domain deploys
 });
